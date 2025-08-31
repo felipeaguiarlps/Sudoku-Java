e65793b4ee2051efe0903fdae71e4fc7fc0f494c
Felipe Aguiar Lopes <felipe.aguiar96@hotmail.com>
2025-08-31 16:53:45 -0300

    Add files

---
+++ demo/src/main/java/com/br/fal/demo/SudokuJavaApplication.java
@@ -0,0 +1,13 @@
+package com.br.fal.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SudokuJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SudokuJavaApplication.class, args);
+	}
+
+}
---
+++ demo/src/test/java/com/br/fal/demo/SudokuJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.fal.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SudokuJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
